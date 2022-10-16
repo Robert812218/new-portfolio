e67aaa8e1ce29393712b362f6de7ec732ae1d49b
Robert812218 <robert812@shaw.ca>
2022-10-15 18:40:00 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import Header from "../components/shared/Header";
+import 'bootstrap/dist/css/bootstrap.css';
+import "../styles/main.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Header from "../components/shared/Header";
 import Link from "next/link";
 import BaseLayout from "../components/layouts/BaseLayout";
-
+import { Container, Button } from "reactstrap";
 
 export default function Home()  {
 	return (
 		<BaseLayout>
-			<h1>INDEX PAGE</h1>
+			<Container>
+				<Button>DANGER</Button>
+			</Container>
 		</BaseLayout>
 	);
 }
