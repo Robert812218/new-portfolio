edbe40252b9a55edd200e6a0975f5a734453bff6
Robert812218 <robert812@shaw.ca>
2022-10-16 17:39:01 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Header from "../components/shared/Header";
 import 'bootstrap/dist/css/bootstrap.css';
-import "../styles/main.css";
+import "../styles/main.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
